5c99848fea50f0cea97bad2c1babb277bdc9c700
tasabina <32531721+tasabina@users.noreply.github.com>
2018-07-25 21:25:17 +1200

    Update config_db.php

--- config/config_db.php
+++ config/config_db.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'dsn' => 'mysql:host=localhost;dbname=id6291734_test;charset=utf8;',
-    'user' => 'id6291734_root',
-    'password' => 'a*1234567',
+    'dsn' => 'mysql:host=localhost;dbname=;charset=utf8;',
+    'user' => '',
+    'password' => '',
 ];
