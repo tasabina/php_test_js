ab2f6feea508159bebe43995133597a9f5576089
tasabina <32531721+tasabina@users.noreply.github.com>
2022-02-08 15:39:32 +1300

    Update Model.php
    Update Core / Model

--- vendor/core/Model.php
+++ vendor/core/Model.php
@@ -2,9 +2,6 @@
 
 class Model
 {
-    public $data = [];
-    public $jdata = [];
-
     public function __construct()
     {
 
@@ -17,37 +14,7 @@ class Model
             exit ('We don\'t have connection...' );
         }
     }
-    
-    public function get_data()
-    {
-        $data = array_map("str_getcsv", file(DATA."customers.csv",FILE_SKIP_EMPTY_LINES));
-        $keys = array_shift($data);
-        foreach ($data as $i=>$row) {
-            $data[$i] = array_combine($keys, $row);
-        }
-        return $this->data=$data;
-    }
 
-    public function save_data(){
-        $this->get_data();
+    function index(){}
 
-        foreach ($this->data as $row)
-        {
-            $tbl = R::dispense('test');
-            foreach ($row as $key=>$item){
-                $key = str_replace('id', 'user_id',$key);
-                $tbl->{$key} = $item;
-            }
-            $tbl = R::store($tbl);
-        }
-    }
-
-    public function load_data(){
-
-        $this->jdata = R::findAll('test');
-        $newdata = json_encode($this->jdata);
-        $file = fopen(DATA.'data.json', 'w');
-        fwrite($file, $newdata);
-        fclose($file);
-    }
 }
